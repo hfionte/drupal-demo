26e450cb56379f3ca7a9eb504f877430ca61d151
Holly Fionte <holly@Hollys-MacBook-Pro-2.local>
2013-10-23 10:00:01 -0400

    new responsive styles

--- sites/all/themes/bowst/js/main.js
+++ sites/all/themes/bowst/js/main.js
@@ -1,1 +1,8 @@
+jQuery(document).ready(function($) {
 
+	// Main Menu Mobile
+	$('#mobile-menu-trigger').live('click', function () {
+		$('#navigation #block-system-main-menu ul.menu').toggleClass('mobile-open');
+	});
+
+});
